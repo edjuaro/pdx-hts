393222f217d9335ecb296bbac1f509ded21844f5
edjuaro <edjuaro@gmail.com>
2018-12-13 12:53:35 -0800

    adding raw data using Git LFS

--- run_notebook.sh
+++
@@ -1,4 +0,0 @@
-echo "docker run --rm -v $PWD/pdx-hts:/pdx-hts -p 8888:8888 -e GRANT_SUDO=yes edjuaro/pdx-hts:1.0 /pdx-hts/launch_jupyter.sh"
-# To run a notebook
-# docker run --rm -v $PWD/mb-pdx-hts:/mb-pdx-hts -p 8888:8888 -e GRANT_SUDO=yes edjuaro/molecular-tumor-board:6.0 /mb-pdx-hts/launch_jupyter.sh
-docker run --rm -v $PWD/pdx-hts:/pdx-hts -p 8888:8888 -e GRANT_SUDO=yes edjuaro/pdx-hts:1.0 /pdx-hts/launch_jupyter.sh
